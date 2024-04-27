7325aa86f9975d497c518cdfc7264851f4588384
Harry Schwartz <harrison.r.schwartz@gmail.com>
2024-04-26 18:47:34 -0700

    DND Classes

--- dnd.js
+++ dnd.js
@@ -2,8 +2,20 @@
 
 
 // preset classes
-
-
+const dndClasses = [
+    "Barbarian",
+    "Bard",
+    "Cleric",
+    "Druid",
+    "Fighter",
+    "Monk",
+    "Paladin",
+    "Ranger",
+    "Rogue",
+    "Sorcerer",
+    "Warlock",
+    "Wizard"
+];
 
 // preset races
 
